3e940b0ac4b632d5a68df659b3e5ebff29bf58ff
Rim4i4ok <rim4i4ok@gmail.com>
2023-03-13 01:47:36 +0300

    Adding styles to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { logo } from "../assets";
+
+import { styles } from "../styles";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [active, setActive] = useState<string>("");
+
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="object-contain w-9 h-9" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Rim <span className="sm:block hidden">| Javascript developer</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
