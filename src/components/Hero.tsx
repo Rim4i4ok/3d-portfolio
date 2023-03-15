ade0d46321cb39885811197928f60cb355b333b1
Rim4i4ok <rim4i4ok@gmail.com>
2023-03-15 23:23:39 +0300

    Adding motion link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -26,6 +27,22 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full mb-1 bg-secondary"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
