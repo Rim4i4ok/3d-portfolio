0430c2eb1af2a82a01073148352e293df4e24acc
Rim4i4ok <rim4i4ok@gmail.com>
2023-03-15 23:31:32 +0300

    Adding computer resize for mobile

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -1,10 +1,10 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { FC, Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers: FC<{ isMobile: boolean }> = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -21,8 +21,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.7}
-        position={[0, -3, -2.2]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3, -2.2]}
         rotation={[-0.01, -0.2, -0.1]}
         intensity={1}
         castShadow
@@ -32,6 +32,21 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => setIsMobile(event.matches);
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () =>
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
@@ -45,7 +60,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
