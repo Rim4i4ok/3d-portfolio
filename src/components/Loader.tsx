cbd34e9504df400937a7c68d81191a5a4b06cbe1
Rim4i4ok <rim4i4ok@gmail.com>
2023-03-15 23:09:00 +0300

    Changing loader for sceen

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,15 @@
+import { Html, useProgress } from "@react-three/drei";
 import React from "react";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  const { progress } = useProgress();
+
+  return (
+    <Html>
+      <span className="canvas-load"></span>
+      <p className="text-[#f1f1f1] text-sm mt-10">{progress.toFixed(2)}%</p>
+    </Html>
+  );
 };
 
 export default Loader;
